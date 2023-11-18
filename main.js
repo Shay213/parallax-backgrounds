d593606f9192d8c396095796d3909d261281f216
shay213 <dawidpawelec@o2.pl>
2023-11-18 11:05:54 +0100

    Add scroll single img inside canvas

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = canvas.width = 800
+const CANVAS_HEIGHT = canvas.height = 700
+
+const backgroundLayer1 = new Image()
+backgroundLayer1.src = 'assets/backgroundLayers/layer-1.png'
+const backgroundLayer2 = new Image()
+backgroundLayer2.src = 'assets/backgroundLayers/layer-2.png'
+const backgroundLayer3 = new Image()
+backgroundLayer3.src = 'assets/backgroundLayers/layer-3.png'
+const backgroundLayer4 = new Image()
+backgroundLayer4.src = 'assets/backgroundLayers/layer-4.png'
+const backgroundLayer5 = new Image()
+backgroundLayer5.src = 'assets/backgroundLayers/layer-5.png'
+
+let gameSpeed = 5
+let x = 0
+
+function animate(){
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  ctx.drawImage(backgroundLayer4, x, 0)
+  x -= gameSpeed
+  requestAnimationFrame(animate)
+}
+
+animate()
