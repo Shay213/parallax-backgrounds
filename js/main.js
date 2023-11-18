1d1d213d142ad479ef4b1f7c966591b041680166
shay213 <dawidpawelec@o2.pl>
2023-11-18 13:00:38 +0100

    Add all layers move at different speed

--- js/main.js
+++ js/main.js
@@ -6,19 +6,23 @@ const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
 
-let gameSpeed = 15
+let gameSpeed = 10
 
-const layer1 = new Layer('assets/backgroundLayers/layer-1.png', 1, gameSpeed)
-const layer2 = new Layer('assets/backgroundLayers/layer-2.png', 1, gameSpeed)
-const layer3 = new Layer('assets/backgroundLayers/layer-3.png', 1, gameSpeed)
-const layer4 = new Layer('assets/backgroundLayers/layer-4.png', 1, gameSpeed)
-const layer5 = new Layer('assets/backgroundLayers/layer-5.png', 1, gameSpeed)
+const layers = [
+  new Layer('assets/backgroundLayers/layer-1.png', 0.2, gameSpeed),
+  new Layer('assets/backgroundLayers/layer-2.png', 0.4, gameSpeed),
+  new Layer('assets/backgroundLayers/layer-3.png', 0.6, gameSpeed),
+  new Layer('assets/backgroundLayers/layer-4.png', 0.8, gameSpeed),
+  new Layer('assets/backgroundLayers/layer-5.png', 1, gameSpeed),
+]
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-  layer4.draw(ctx)
-  layer4.update()
+  layers.forEach(layer => {
+    layer.update()
+    layer.draw(ctx)
+  })
 
   requestAnimationFrame(animate)
 }
